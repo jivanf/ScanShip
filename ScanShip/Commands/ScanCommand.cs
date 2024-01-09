743b62c4305863c4e53dee6583c9f806c0c278d8
Jivan Flores <jivan.cortier@gmail.com>
2024-01-09 00:15:22 -0800

    Let's get this started

---
+++ ScanShip/Commands/ScanCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using LethalAPI.LibTerminal.Attributes;
+using UnityEngine;
+
+namespace ScanShip.Commands;
+
+public class ScanCommand
+{
+    [TerminalCommand("Scan", true)]
+    public string Scan(string location)
+    {
+        if (location != "ship")
+        {
+            return null;
+        }
+
+        var scrapObjectsInShip = Object
+            .FindObjectsOfType<GrabbableObject>()
+            .Where(scrapObject => scrapObject.itemProperties.isScrap && scrapObject.isInShipRoom)
+            .ToList();
+
+        int totalValue = scrapObjectsInShip.Sum(scrapObject => scrapObject.scrapValue);
+        int count = scrapObjectsInShip.Count;
+
+        return $"There {(count == 1 ? "is" : "are")} {count} {(count == 1 ? "object" : "objects")} inside the ship, totalling at a value of ${totalValue}.";
+    }
+}
---
+++ ScanShip/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using LethalAPI.LibTerminal;
+using LethalAPI.LibTerminal.Models;
+using ScanShip.Commands;
+
+namespace ScanShip
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(LethalAPI.LibTerminal.PluginInfo.PLUGIN_GUID)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private TerminalModRegistry commandRegistry;
+
+        private void Awake()
+        {
+            this.commandRegistry = TerminalRegistry.CreateTerminalRegistry();
+            this.commandRegistry.RegisterFrom<ScanCommand>();
+        }
+
+        private void OnDestroy()
+        {
+            this.commandRegistry.Deregister();
+        }
+    }
+}
